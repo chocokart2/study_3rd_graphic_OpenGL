f87c855616a60aec013f798c01fa975c44dfcae8
chocokart2 <chocokart@naver.com>
2024-06-11 13:41:54 +0900

    2024-06-11 파일 생성

---
+++ Projects/Chocokart2/2024-06-23-assignment/no2024-06-23-assignmentTry1/no2024-06-23-assignmentTry1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace no2024_06_23_assignmentTry1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ Projects/Chocokart2/2024-06-23-assignment/no2024_06_23_assignmentTry2/no2024_06_23_assignmentTry2/no2024_06_23_assignmentTry2.cpp
@@ -0,0 +1,43 @@
+﻿// no2024_06_23_assignmentTry2.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+#include <GL/glut.h>
+#include <GL/gl.h>
+#include <iostream>
+
+
+
+void DrawTriangle()
+{
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glBegin(GL_TRIANGLES);
+    glVertex2f(0.0, 0.5);
+    glVertex2f(-0.5, -0.5);
+    glVertex2f(0.5, -0.5);
+    glEnd();
+    glFlush();
+}
+
+
+
+void main(int* a, char** argv)
+{
+    glutInit(a, argv);
+    glutCreateWindow("OpenGL");
+    glutDisplayFunc(DrawTriangle);
+    glutMainLoop();
+
+}
+
+
+
+// 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
+// 프로그램 디버그: <F5> 키 또는 [디버그] > [디버깅 시작] 메뉴
+
+// 시작을 위한 팁: 
+//   1. [솔루션 탐색기] 창을 사용하여 파일을 추가/관리합니다.
+//   2. [팀 탐색기] 창을 사용하여 소스 제어에 연결합니다.
+//   3. [출력] 창을 사용하여 빌드 출력 및 기타 메시지를 확인합니다.
+//   4. [오류 목록] 창을 사용하여 오류를 봅니다.
+//   5. [프로젝트] > [새 항목 추가]로 이동하여 새 코드 파일을 만들거나, [프로젝트] > [기존 항목 추가]로 이동하여 기존 코드 파일을 프로젝트에 추가합니다.
+//   6. 나중에 이 프로젝트를 다시 열려면 [파일] > [열기] > [프로젝트]로 이동하고 .sln 파일을 선택합니다.
